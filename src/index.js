69b4164c233bef83038733e67f471f82fabc3813
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-03-24 17:29:33 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let addToy = false;
+const input = document.getElementsByClassName('input-text')
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +14,58 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+
+
+function fetchToys(name, image){
+  console.log("in fetch toys")
+  let formData = {
+    name,
+    image,
+    likes: 0 
+  }
+  console.log(formData)
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+
+  return fetch("http://localhost:3000/toys", configObj)
+  .then(resp => resp.json())
+  .then(json => { console.log(json)
+    json.forEach(e => {
+      const p = document.createElement('p')
+      const btn = document.createElement("BUTTON")
+      btn.classList.add("like-btn")
+      btn.innerText = "Like"
+      p.innerText = e['likes']
+      const img = document.createElement('img')
+      img.src = e['image']
+      img.classList.add("toy-avatar")
+      const h2 = document.createElement('h2')
+      h2.innerText = e['name']
+      const otherDiv = document.getElementById('toy-collection')
+      const div = document.createElement('div')
+      div.classList.add("card")
+      otherDiv.appendChild(div)
+      div.appendChild(h2)
+      div.appendChild(img)
+      div.appendChild(p)
+      div.appendChild(btn)
+    }
+      )
+  })
+}
+
+const submit = document.getElementsByClassName("submit")
+submit[0].addEventListener('click', e => {
+  console.log("hi")
+  fetchToys(input[0].value, input[1].value)
+  e.preventDefault()
+})
+
